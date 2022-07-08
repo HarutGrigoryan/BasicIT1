4094e1e091f14ba77649c8c2581673eba0f1cb30
PC <PC@DESKTOP-EKT876A>
2022-07-08 19:20:35 +0400

    Add project files.

---
+++ Lesson 1/Program.cs
@@ -0,0 +1,271 @@
+﻿//int a = 1;
+//int b = 2;
+//int c = 3;
+
+//21.
+//if (a > b && a > c)
+//{
+//    Console.WriteLine(a);
+//}
+//else if (b > a && b > c)
+//{
+//    Console.WriteLine(b);
+//}
+//else
+//{
+//    Console.WriteLine(c);
+//}
+
+//22.
+
+//if (a < b && a < c)
+//{
+//    Console.WriteLine(a);
+//}
+//else if (b < a && b < c)
+//{
+//    Console.WriteLine(b);
+//}
+//else
+//{
+//    Console.WriteLine(c);
+//}
+
+//23.
+
+//if ( a == 1 || b == 1 || c == 1)
+//{
+//    Console.WriteLine(true);
+//}
+//else
+//{
+//    Console.WriteLine(false);
+//}
+
+//24.
+
+//if (a == 2 && b == 2)
+//{
+//    Console.WriteLine(true);
+//}
+//else if ( a == 2 && c == 2)
+//{
+//    Console.WriteLine(true);
+//}
+//else if ( b == 2 && c == 2)
+//{
+//    Console.WriteLine(true);
+//}
+//else
+//{
+//    Console.WriteLine(false);
+//}
+
+//25.
+
+//int y;
+
+//if (a * a + b * b == c * c)
+//{
+//    Console.WriteLine("y = 1");
+//}
+//else if ( a * a + c * c == b * b)
+//{
+//    Console.WriteLine("y = 1");
+//}
+//else if (b * b + c * c == a * a)
+//{
+//    Console.WriteLine("y = 1");
+//}
+//else
+//{
+//    Console.WriteLine("y = 2");
+//}
+
+//26.
+
+//if (a % 2 == 0)
+//{
+//    Console.WriteLine(1);
+//}
+//else if (b % 2 == 0)
+//{
+//    Console.WriteLine(1);
+//}
+//else if (c % 2 == 0)
+//{
+//    Console.WriteLine(1);
+//}
+//else
+//{
+//    Console.WriteLine(2);
+//}
+
+//27.
+
+//28.
+
+//29.
+//int minnum;
+//int midnum;
+//int maxnum;
+
+//if ( a > b && a > c)
+//{
+//    maxnum = a;
+//}
+//else if ( b > c && b > a )
+//{
+//    maxnum = b;    
+//}
+//else
+//{
+//    maxnum = c;
+//}
+
+//if (a > b && a < c)
+//{
+//    midnum = a;
+//}
+//else if (b > a && b < c)
+//{
+//    midnum = b;
+//}
+//else
+//{
+//    midnum = c;
+//}
+
+
+//if ( a < b && a < c )
+//{
+//    minnum = a;
+//}
+//else if (b < a && b <c)
+//{
+//    minnum = b;
+//}
+//else
+//{
+//    minnum = c;
+
+//}
+
+//Console.WriteLine(minnum);
+//Console.WriteLine(midnum);
+//Console.WriteLine(maxnum);
+
+
+//30.
+
+
+
+
+
+
+
+
+
+
+
+//31.
+
+//int a = 4;
+//int b = 4;
+//int c = 4;
+//int d = 12;
+
+//if ( a > b && a > c && a >d)
+//{
+//    Console.WriteLine(a);
+//}
+//else if ( b > a && b > c && b > d)
+//{
+//    Console.WriteLine(b);
+//}
+//else if (c > a && c > b && c > d)
+//{
+//    Console.WriteLine(c);
+//}
+//else
+//{
+//    Console.WriteLine(d);
+//}
+
+
+
+//32.
+
+//if ( a < b && a < c && a < d)
+//{
+//    Console.WriteLine(a);
+//}
+//else if (b < a && b < c && b < d)
+//{
+//    Console.WriteLine(b);
+//}
+//else if ( c < a && c < b && c < d)
+//{
+//    Console.WriteLine(c);
+//}
+//else
+//    {
+//        Console.Write(d);
+//    }
+
+
+
+
+//33.
+
+//if ( a == 1 || b == 1 || c == 1 || d == 1)
+//{
+//    Console.WriteLine(true);
+//}
+//else
+//{
+//    Console.WriteLine(false);
+//}
+
+//34.
+
+//if (a + b == c + d)
+//{
+//    Console.WriteLine(true);
+//}
+//else if (a + c == b + d)
+//{
+//    Console.WriteLine(true);
+//}
+//else if ( a + d == b + c)
+//{
+//    Console.WriteLine(true);
+//}
+//else
+//{
+//    Console.WriteLine(false);
+//}
+
+
+//35.
+
+//if (a == b + c + d)
+//{
+//    Console.WriteLine(true);
+//}
+//else if (b == a + c + d)
+//{
+//    Console.WriteLine(true);
+//}
+//else if (c == a + b + d)
+//{
+//    Console.WriteLine(true);
+//}
+//else if (d == a + c + b)
+//{
+//    Console.WriteLine(true);
+//}
+//else
+//{
+//    Console.WriteLine(false);
+//}
---
+++ Lesson 2/Program.cs
@@ -0,0 +1,91 @@
+﻿//1
+
+//for (int i = 1; i <= 10; i++)
+//{
+//    Console.WriteLine(i);
+//}
+
+
+//2
+
+//int i; int sum = 0;
+
+//for ( i = 1; i <= 10; i++)
+//{
+//    sum=sum+i;
+//    Console.WriteLine(i);
+//}
+//Console.WriteLine(sum);
+
+//3
+
+//int i; int sum = 0;
+//for(i = 1; i <= 7; i++)
+//{
+//    sum = sum + i;
+//    Console.WriteLine(i);
+//}
+//Console.WriteLine(sum);
+
+//4
+//int i;int n;int sum = 0;
+//n = Convert.ToInt32(Console.ReadLine());
+//for (i = 0; i <= n; i++)
+//{
+//    sum += i;
+//    Console.WriteLine(i);
+//}
+//Console.WriteLine(sum);
+
+//5
+
+//int n;int sum = 0;
+
+//Console.Write("input 10 number");
+//Console.WriteLine();
+//for (n = 1;n <= 10; n++)
+//{
+//    Convert.ToInt32(Console.ReadLine());
+//    sum = sum + n;
+//}
+//Console.WriteLine(sum);
+
+
+//6
+//int i; int n; int sum = 0;
+//double avg;
+
+//Console.Write("\n\n");
+//Console.Write("Read 10 numbers and calculate sum and average:\n");
+//Console.Write("----------------------------------------------");
+//Console.Write("\n\n");
+
+//Console.Write("Input the 10 numbers : \n");
+//for (i = 1; i <= 10; i++)
+//{
+//	Console.Write("Number-{0} :", i);
+
+//	n = Convert.ToInt32(Console.ReadLine());
+//	sum += n;
+//}
+//avg = sum / 10.0;
+//Console.Write("The sum of 10 no is : {0}\nThe Average is : {1}\n", sum, avg);
+
+//7
+
+//int i; int ctr;
+
+//Console.WriteLine("Display the cube of the number");
+
+//Console.Write("---------------------------------");
+//Console.Write("\n\n");
+//Console.Write("Input number of terms : ");
+
+//ctr = Convert.ToInt32(Console.ReadLine());
+
+//for (i = 0; i <= ctr; i++)
+//{
+//	Console.Write("Number is : {0} and cube of the {1} is :{2} \n", i, i, (i * i * i));
+//}
+
+
